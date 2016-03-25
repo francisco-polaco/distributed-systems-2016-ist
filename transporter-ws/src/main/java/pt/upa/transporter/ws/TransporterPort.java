8ffbb4f8f2bd450f0bc64232e4e4c8e991ee8cc8
Francisco Santos <franciscopolaco@tecnico.ulisboa.pt>
2016-03-25 19:00:28 +0000

    POM is updated with uiid dependency. Transponder server is now ready to implement.

--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,7 +1,46 @@
 package pt.upa.transporter.ws;
 
-public class TransporterPort {
+import javax.jws.WebService;
+import java.util.List;
 
-	// TODO
+@WebService(
+        endpointInterface="pt.upa.transporter.ws.TransporterPortType",
+        wsdlLocation="transporter.1_0.wsdl",
+        name="TransporterWebService",
+        portName="TransporterPort",
+        targetNamespace="http://ws.transporter.upa.pt/",
+        serviceName="TransporterService"
+)
+public class TransporterPort implements TransporterPortType{
+    @Override
+    public String ping(String name) {
+        return name;
+    }
+
+    @Override
+    public JobView requestJob(String origin, String destination, int price) throws BadLocationFault_Exception, BadPriceFault_Exception {
+        return null;
+    }
+
+    @Override
+    public JobView decideJob(String id, boolean accept) throws BadJobFault_Exception {
+        return null;
+    }
+
+    @Override
+    public JobView jobStatus(String id) {
+        return null;
+    }
+
+    @Override
+    public List<JobView> listJobs() {
+        return null;
+    }
+
+    @Override
+    public void clearJobs() {
+
+    }
+
 
 }
