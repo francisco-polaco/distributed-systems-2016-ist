dc7639ac874993a16608a3794147cc54e686c74c
Radaeld <joao.serras@ist.utl.pt>
2016-05-11 22:28:18 +0100

    Invalid timestamp exception

---
+++ ws-handlers/src/main/java/pt/upa/handler/InvalidTimestampSOAPException.java
@@ -0,0 +1,21 @@
+package pt.upa.handler;
+
+/**
+ * Created by jp_s on 5/10/2016.
+ */
+public class InvalidTimestampSOAPException extends RuntimeException {
+
+        private String mInfo;
+
+        public InvalidTimestampSOAPException(String info){
+            super();
+            mInfo = info;
+        }
+
+        @Override
+        public String getMessage(){
+            return "Invalid Timestamp:\n" + mInfo;
+        }
+}
+
+
